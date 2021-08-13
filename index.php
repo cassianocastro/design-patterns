7a0e83a8a3f7b108242c2a8e047ac36d2c45941b
cassianocastro <tottenham248@gmail.com>
2021-08-13 13:26:57 -0300

    Alteração para usar a Fachada dos Padrões Criacionais

--- index.php
+++ index.php
@@ -1,76 +1,59 @@
-<?php
-use Creational\{
-    FactoryMethod\Test as FactoryMethod,
-    SimpleFactory\Bicycle,
-    SimpleFactory\SimpleFactory,
-    Singleton\Singleton,
-    StaticFactory\Test as StaticFactory,
-    AbstractFactory\Test as AbstractFactory,
-    AbstractFactory\UnixWriterFactory,
-    AbstractFactory\WindowsWriterFactory,
-    Pool\Test as Pool,
-    Prototype\Test as Prototype
-};
+<!DOCTYPE html>
+<html lang="pt-BR" dir="ltr">
+    <head>
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="author" content="Cassiano R. Castro">
+        <title>Design Patterns</title>
+    </head>
+    <body>
+        <header>
+            <h1>Design Patterns</h1>
+        </header>
+        <main>
+            <article>
+                <section>
+                    <h2>Creational</h2>
+                    <pre>
+                        <?php
+                            use Creational\CreationalFacade;
 
-use Comportamental\NullObject\Test as NullObject;
-use Estrutural\DependencyInjection\Test as DI;
+                            ini_set('display_errors', 1);
+                            ini_set('display_startup_errors', 1);
+                            error_reporting(E_ALL);
 
-ini_set('display_errors', 1);
-ini_set('display_startup_erros', 1);
-error_reporting(E_ALL);
-
-require_once './Loader.php';
-
-echo "<pre>";
-
-echo "<h2>FactoryMethod:</h2>";
-$factoryMethodTest = new FactoryMethod();
-$factoryMethodTest->canCreateStdoutLogging();
-$factoryMethodTest->canCreateFileLogging();
-
-echo "<h2>SimpleFactory:</h2>";
-$bicyle = (new SimpleFactory())->createBicycle();
-$bicyle->driveTo("Porto Alegre");
+                            require_once './Loader.php';
 
 
-echo "<h2>Singleton:</h2>";
-$firstCall  = Singleton::getInstance();
-$secondCall = Singleton::getInstance();
+                        ?>
+                    </pre>
+                </section>
+                <section>
+                    <h2>Comportamental</h2>
+                    <pre>
+                        <?php
+                            use Comportamental\NullObject\Test as NullObject;
 
-if ($firstCall === $secondCall):
-    echo "Mesma intância.";
-endif;
+                            echo "<h3>Object NULL:</h3>";
+                            $nullObject = new NullObject();
+                            $nullObject->testNullObject();
+                            $nullObject->testStdLogger();
+                        ?>
+                    </pre>
+                </section>
+                <section>
+                    <h2>Estrutural</h2>
+                    <pre>
+                        <?php
+                            use Estrutural\DependencyInjection\Test as DI;
 
-echo "<h2>StaticFactory:</h2>";
-$staticFactoryTest = new StaticFactory();
-$staticFactoryTest->testCanCreateNumberFormatter();
-$staticFactoryTest->testCanCreateStringFormatter();
-$staticFactoryTest->testException();
-
-echo "<h2>AbstractFactory:</h2>";
-$abstractFactory = new AbstractFactory();
-print "Unix:\n";
-$abstractFactory->canCreateCSVwriter(new UnixWriterFactory());
-print "Windows:\n";
-$abstractFactory->canCreateCSVwriter(new WindowsWriterFactory());
-
-echo "<h2>Pool:</h2>";
-$pool = new Pool();
-$pool->canGetNewInstanceWithGet();
-$pool->canGetSameInstanceTwiceWhenDisposingItFirst();
-
-echo "<h2>Prototype:</h2>";
-$proto = new Prototype();
-$proto->canGetFooBook();
-
-echo "<h2>Object NULL:</h2>";
-$nullObject = new NullObject();
-$nullObject->testNullObject();
-$nullObject->testStdLogger();
-
-echo "<h2>Dependency Injection:</h2>";
-$di = (new DI())->testDependencyInjection();
-
-
-echo "</pre>";
-?>
+                            echo "<h3>Dependency Injection:</h3>";
+                            $di = (new DI())->testDependencyInjection();
+                        ?>
+                    </pre>
+                </section>
+            </article>
+        </main>
+    </body>
+</html>
